11dc14054b692ea578a03f60db3d9ebe2e4b0690
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2017-04-12 23:57:17 +0600

    Removed debug action dispatch in app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 
-import * as actions from 'actions';
 import {configure} from 'configureStore';
 const store = configure();
 
 store.subscribe(() => {
   console.log('new state', store.getState());
 });
-
-store.dispatch(actions.addTodo('go'));
 
 import TodoApp from 'TodoApp';
 
