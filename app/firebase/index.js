a50e8174f9b93e1b90e15bb75efb633237bca670
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2017-04-15 20:36:44 +0600

    Getting firebase credentials from env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,13 +1,12 @@
 import firebase from 'firebase';
-
 try {
   const config = {
-    apiKey: "AIzaSyCgspj_9PSugBiMP4abt9zyM8x9IS11XzA",
-    authDomain: "naffiq-todoapp.firebaseapp.com",
-    databaseURL: "https://naffiq-todoapp.firebaseio.com",
-    projectId: "naffiq-todoapp",
-    storageBucket: "naffiq-todoapp.appspot.com",
-    messagingSenderId: "624480386114"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch (e) {
